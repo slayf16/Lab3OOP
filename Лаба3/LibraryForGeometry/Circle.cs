42429c5be5e6ebf7ada4f36aec4a1dd6231b0ebb
Alexey Kozyrev <tcsid4@gmail.com>
2021-10-29 19:34:50 +0700

    Лабораторная работа 1: добавлена сущность-интерфейс геометрические фигуры, создан класс Окружность и метод для вычисления площади окружности

---
+++ Лаба3/LibraryForGeometry/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryForGeometry
+{
+    public class Circle : IFigure
+    {
+        
+        /// <summary>
+        /// поле
+        /// </summary>
+        private double _radius;
+
+        public double Radius
+        {
+            get
+            {
+                return _radius;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    _radius = value;
+                }
+                else
+                {
+                    throw new Exception("Радиус должен быть больше нуля");
+                }
+            }
+        }
+
+        public double Square ()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+       
+
+    }
+}
