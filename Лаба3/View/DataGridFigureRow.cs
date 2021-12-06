a09ba6e4491a8a8c48acdbf5ef3ac284f78792b5
Alexey Kozyrev <tcsid4@gmail.com>
2021-12-06 11:52:26 +0700

    Добавлены классы: WorkWithFile для сохранения и загрузки данных, и DatagridFigureRow для создания структуры таблицы

---
+++ Лаба3/View/DataGridFigureRow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    /// <summary>
+    /// Класс для формирования структуры таблицы
+    /// </summary>
+    public class DataGridFigureRow
+    {
+        /// <summary>
+        /// переменная для записи параметров фигуры
+        /// </summary>
+        public string FigureProps { get; set; }
+
+        /// <summary>
+        /// переменная для записи имени фигуры
+        /// </summary>
+        public string FigureName { get; set; }
+
+    }
+
+}
---
+++ Лаба3/View/WorkIWithFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    /// <summary>
+    /// работа (сохранение/загрузка) с файлами
+    /// </summary>
+    public class WorkIWithFile
+    {
+        /// <summary>
+        /// сохранение файлы путем XML-серилизации
+        /// </summary>
+        /// <param name="path">путь до файла</param>
+        public static void SaveFile(string path,
+            System.ComponentModel.BindingList<DataGridFigureRow> data)
+
+        {
+            var writer = new System.Xml.Serialization.XmlSerializer(typeof
+                (System.ComponentModel.BindingList<DataGridFigureRow>));
+
+            using (var file = System.IO.File.Create(path))
+            {
+                writer.Serialize(file, data);
+                file.Close();
+            }
+        }
+
+        /// <summary>
+        /// метод для загрузки файла
+        /// </summary>
+        /// <param name="path">путь до файла</param>
+        /// <returns>Возвращает загруженный список</returns>
+        public static System.ComponentModel.BindingList<DataGridFigureRow> LoadFile(string path)
+        {
+            System.ComponentModel.BindingList<DataGridFigureRow> data = new
+                System.ComponentModel.BindingList<DataGridFigureRow>();
+
+            var reader = new System.Xml.Serialization.XmlSerializer(typeof
+                (System.ComponentModel.BindingList<DataGridFigureRow>));
+
+            var file = new System.IO.StreamReader(path);
+            data = (System.ComponentModel.BindingList<DataGridFigureRow>)reader.Deserialize(file);
+            return data;
+        }
+    }
+}
