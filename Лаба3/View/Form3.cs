66b0585576e868ee44614793c807297406ebc4ba
Alexey Kozyrev <tcsid4@gmail.com>
2021-12-06 12:55:33 +0700

    Form3: метод для поиска совпадений в массиве, обработка нажатий кнопок (ОК, отмена)

--- Лаба3/View/Form3.cs
+++ Лаба3/View/Form3.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace View
 {
+    /// <summary>
+    /// форма для выполнения поиска данных
+    /// </summary>
     public partial class Form3 : Form
     {
         /// <summary>
@@ -17,9 +20,53 @@ namespace View
         /// </summary>
         public FigureForm ParentForm { get; set; }
 
+        /// <summary>
+        /// конструктор формы
+        /// </summary>
         public Form3()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Кнопка для выполнения поиска
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("введите условия для выполнения поиска");
+            }
+            else
+            {
+                List<int> index = new List<int>();
+                int i = 0;
+                var parent = this.ParentForm as FigureForm;
+                foreach (DataGridFigureRow str in parent.FigureList)
+                {
+                    if (str.FigureName.Contains(textBox1.Text) ||
+                        str.FigureProps.Contains(textBox1.Text))
+                    {
+                        index.Add(i);
+                    }
+                    i++;
+                }
+                parent.SelectRow(index);
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// кнопка для отказа от поиска
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
